af2316a3f720cfcb2cea18494efba66367d5f154
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-08-22 21:45:45 +0300

    edit the name of the image

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,13 @@ docker stop python-app || true
 docker rm python-app || true
 
 # Remove used image
-docker rmi ahmedalaa14/flask-app-min || true
+docker rmi ahmedalaa14/flask-app-mini || true
 
 # Build new image
-docker build -t ahmedalaa14/flask-app-min .
+docker build -t ahmedalaa14/flask-app-mini .
 
 # Push docker image
-docker push ahmedalaa14/flask-app-min
+docker push ahmedalaa14/flask-app-mini
 
 # Run new container
-docker run -d --name python-app -p 5000:5000 ahmedalaa14/flask-app-min
+docker run -d --name python-app -p 5000:5000 ahmedalaa14/flask-app-mini
