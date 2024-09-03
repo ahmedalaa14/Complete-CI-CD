c832c3e1619a4b3d101c3e99288fed9570e5be47
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-09-03 20:54:00 +0300

    variables for ec2 module

---
+++ terraform/ec2/variables.tf
@@ -0,0 +1,10 @@
+variable "sg_vpc_id"{
+    description = "VPC ID"
+    type = string
+}
+
+variable "ec2_subnet_id" {
+    description = "Subnet ID where instance will be launched"
+    type = string
+}
+
