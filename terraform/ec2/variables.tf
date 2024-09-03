acd30a3cf4258f09df6e3ad043eb16b5ce7250ed
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-09-03 21:12:46 +0300

    variables.tf ec2 module done

--- terraform/ec2/variables.tf
+++ terraform/ec2/variables.tf
@@ -1,10 +1,15 @@
+# Security Group VPC ID
 variable "sg_vpc_id"{
     description = "VPC ID"
     type = string
 }
-
+# ec2 subnet id where instance will be launched
 variable "ec2_subnet_id" {
     description = "Subnet ID where instance will be launched"
     type = string
 }
-
+# instance profile
+variable "instance_profile"{
+    description = "ec2 instance profile"
+    type = string
+}
