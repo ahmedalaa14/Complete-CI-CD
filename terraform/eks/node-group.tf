188c51ce68baa50d3c631c0c8d8c789ecc8040b8
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-09-03 21:13:29 +0300

    node-group.tf for eks modue

---
+++ terraform/eks/node-group.tf
@@ -0,0 +1,61 @@
+# node group
+resource "aws_eks_node_group" "worker_nodes" {
+  cluster_name    = aws_eks_cluster.eks_cluster.id
+  node_group_name = "team6_node"
+  node_role_arn   = aws_iam_role.eks_nodegroup_role.arn
+  subnet_ids      = var.nodegroup_subnets_id
+  capacity_type   = "ON_DEMAND"
+  disk_size       = "20"
+  ami_type        = "AL2_x86_64"
+  instance_types  = ["t3.micro"]
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 2
+  }
+  update_config {
+    max_unavailable = 1
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.eks-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.eks-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.eks-AmazonEC2ContainerRegistryReadOnly,
+  ]
+  tags = {
+    Name = "team6-worker-Node"
+  }
+}
+
+# node group Role
+resource "aws_iam_role" "eks_nodegroup_role" {
+  name               = "eks-node-group-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+# Associate IAM policies to nodegroup Role 
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_nodegroup_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_nodegroup_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks-AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_nodegroup_role.name
+}
