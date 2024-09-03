c6c1f455d8352e4c3bd7b33512a8f02e0adcc792
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2024-09-03 20:53:42 +0300

    ec2 module

---
+++ terraform/ec2/main.tf
@@ -0,0 +1,83 @@
+# EC2 AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+  
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  owners = ["099720109477"]
+}
+
+# EC2 security group
+resource "aws_security_group" "sg" {
+  vpc_id = var.sg_vpc_id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080     # open port (8080) for jenkins
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22    # allow ssh to configure instance 
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
+}
+
+# EC2 instance
+resource "aws_instance" "jenkins_ec2" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.micro"
+  subnet_id                   = var.ec2_subnet_id    
+  security_groups             = [aws_security_group.sg.id]
+  associate_public_ip_address = true
+  key_name                    = "team6-key"
+  tags = {
+      Name = "jenkins_EC2"
+    } 
+
+  provisioner "local-exec" {
+    when        = create
+    on_failure  = continue
+    command = "echo ${self.public_ip} >> ec2-ip.txt"
+ }
+}
